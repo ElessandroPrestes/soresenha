10bc3c68132b70e51e559b1a781d5faf93220878
Elessandro Prestes Macedo <elessandropm@gmail.com>
2019-05-13 16:34:37 -0300

    Concatenado e minificado arquivos .js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,16 @@
 
 import  gulp from 'gulp';
 import watch from 'gulp-watch';
-import concat from'gulp-concat-css';
+import concss from'gulp-concat-css';
+import conjs from'gulp-concat';
 import notify from'gulp-notify';
 import css from'gulp-clean-css';
 import uglify from 'gulp-uglify-es';
 import rename from 'gulp-rename';
 
-gulp.task('concatcss', () =>{
-    return gulp.src('assets/css/*css')
-    .pipe(concat("style.css"))
+gulp.task('concss', () =>{
+    return gulp.src('assets/css/*.css')
+    .pipe(concss("style.css"))
     .on("error", notify.onError("Error: <%= error.message %>"))
     .pipe(gulp.dest('dist/css/concat/'));
 });
@@ -17,22 +18,28 @@ gulp.task('concatcss', () =>{
 gulp.task('mincss', () =>{
   return gulp.src('dist/css/concat/style.css')
   .pipe(rename('style.min.css'))
-  .pipe(css())
+  .pipe(css({compatibilty: 'ie8'}))
   .on("error", notify.onError("Error: <%= error.message %>"))
   .pipe(gulp.dest('dist/css/minify/'));
 });
 
-
-gulp.task('minjs', () =>{
-  return gulp.src('js/index.js')
-  .pipe(rename('index.min.js'))
-  .pipe(uglify())
+gulp.task('conjs', () => {
+  return gulp.src(['assets/js/jquery.js' ,'assets/js/pooper.js','assets/js/bootstrap.js', 'assets/js/main.js'])
+  .pipe(conjs("main.js"))
   .on("error", notify.onError("Error: <%= error.message %>"))
-  .pipe(gulp.dest('dist/js/'))
+  .pipe(gulp.dest('dist/js/concat'));
 });
 
-gulp.task('default',['concatcss','mincss','minjs'], () => {
-    gulp.watch('assets/css/*css', ['concatcss']);
+gulp.task('minjs', () =>{
+  return gulp.src('dist/js/concat/main.js')
+  .pipe(rename('main.min.js'))
+  .pipe(uglify())
+  .on("error", notify.onError("Error: <%= error.message %>"))
+  .pipe(gulp.dest('dist/js/minify'))
+});
+
+gulp.task('default',['concss','mincss','minjs'], () => {
+    gulp.watch('assets/css/*css', ['concss']);
     gulp.watch('dist/css/concat/style.css', ['mincss']);
     gulp.watch('js/index.js', ['minjs']);
 });
